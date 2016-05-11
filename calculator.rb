644c8034d87829f86f5dcdaa87b8708d7c7aabd7
Pat Marshall <pmarshall1716@gmail.com>
2016-05-11 14:25:07 -0600

    added calculator.rb

---
+++ calculator.rb
@@ -0,0 +1,17 @@
+puts "Welcome to the ruby calculator"
+puts "enter your first number"
+
+#get the first number
+ puts "enter your first number"
+ #gets the first number
+
+ puts "enter your operator"
+ #get the operator
+ puts "enter the second number"
+ #get the second number
+
+ #figure out how to do the math on number
+ #output the result to the user
+
+ #do all the bonus problems
+
