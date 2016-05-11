5c600f59bd19e8608ea3c10913c072d3ecd93f80
Pat Marshall <pmarshall1716@gmail.com>
2016-05-11 13:58:32 -0600

    Final Project

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,17 @@
-puts "first commit"
-puts "second message commit"
-puts "third message commit"
+puts "Welcome to the ruby calculator"
+puts "enter your first number"
+
+#get the first number
+ puts "enter your first number"
+ #gets the first number
+
+ puts "enter your operator"
+ #get the operator
+ puts "enter the second number"
+ #get the second number
+
+ #figure out how to do the math on number
+ #output the result to the user
+
+ #do all the bonus problems
+
